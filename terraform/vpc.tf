18055545c3455ae3c0cdcab0ab40b1071c5cc750
Semgrep Autofix <autofix@semgrep.com>
2026-05-17 06:33:14 +0000

    disable automatic public IP assignment in public subnet
    Disable automatic public IP assignment for resources launched in the public subnet to reduce exposure surface.
    
    ## Changes
    - Set `map_public_ip_on_launch` to `false` in `aws_subnet.public` resource in `terraform/vpc.tf`
    
    ## Why
    Automatically assigning public IP addresses to all resources in a subnet violates the principle of least privilege. Resources that genuinely require public connectivity (such as the NAT gateway) already use explicitly allocated Elastic IPs. This change ensures other resources launched in the subnet are not inadvertently exposed to the public internet.
    
    ## Semgrep Finding Details
    Resources in the AWS subnet are assigned a public IP address. Resources should not be exposed on the public internet, but should have access limited to consumers required for the function of your application. Set `map_public_ip_on_launch` to false so that resources are not publicly-accessible.
    
    cipherpath@proton.me requested this Autofix PR for [this finding](https://semgrep.dev/orgs/johnydev/findings/797164296) from the detection rule [terraform.aws.security.aws-subnet-has-public-ip-address.aws-subnet-has-public-ip-address](https://semgrep.dev/r/terraform.aws.security.aws-subnet-has-public-ip-address.aws-subnet-has-public-ip-address).

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = local.az_primary
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.project_name}-public"
